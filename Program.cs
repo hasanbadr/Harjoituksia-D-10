00118599c131585197e427204564e20521d454d9
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 07:56:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä kokonaisluku väliltä 1 - 10: ");
+        int luku = int.Parse(Console.ReadLine());
+
+        if (luku >= 1 && luku <= 10)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                int tulos = luku * i;
+                Console.WriteLine($"{luku} x {i} = {tulos}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Syötä luku väliltä 1 - 10.");
+        }
+    }
+}
